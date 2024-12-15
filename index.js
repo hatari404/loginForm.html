7509f83ca6c83f38a5056940f484a6d8d5029d2b
eva <evapasternak666@gmail.com>
2024-12-15 14:58:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Server is temporarily unavailable...");
+});
